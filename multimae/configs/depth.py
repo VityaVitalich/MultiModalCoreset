56dae2263db65fdb6634abb86bad436e2b2c43e8
VityaVitalich <enemy1307@mail.ru>
2024-02-12 17:56:02 +0000

    debugging semantic

--- multimae/configs/depth.py
+++ multimae/configs/depth.py
@@ -4,16 +4,16 @@ import ml_collections
 def depth_configs():
     config = ml_collections.ConfigDict()
 
-    config.run_name = "long-rgb-semseg"
+    config.run_name = "debug-only-semseg"
     config.log_dir = "./logs/"
     config.cons_lvl = "INFO"
     config.file_lvl = "INFO"
 
     config.device = "cuda"
 
-    config.in_domains = ["rgb", "semseg"]
+    config.in_domains = ["semseg"]
     config.out_domains = ["depth"]
-    config.decoder_main_tasks = ["rgb", "semseg"]
+    config.decoder_main_tasks = ["semseg"]
 
     config.semseg_num_classes = 256
 
@@ -25,7 +25,7 @@ def depth_configs():
     config.lr = 3e-4
     config.weight_decay = 1e-4
     config.total_epochs = 5
-    config.batch_size = 32
+    config.batch_size = 64
 
     config.train_dir = "../../data/dq/clevr_complex/train"
     config.val_dir = "../../data/dq/clevr_complex/val"
